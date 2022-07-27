98d2f3c0a0d04b30499db3a19f9af7fdb33add20
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-07-27 06:16:50 -0500

    remove trending images from static folder and will just leave small,medium,large versions of each image

--- server/src/apollo/transform-resolvers.ts
+++ server/src/apollo/transform-resolvers.ts
@@ -1,24 +1,30 @@
 import { DbMovie, DbShow } from '../database/db';
 import { Movie, Show } from './resolvers-types.generated';
 
-export const movieTransform = (
-  movie: DbMovie
-): Omit<Movie, 'thumbnailRegular' | 'thumbnailTrending'> => {
+export const movieTransform = (movie: DbMovie): Movie => {
   return {
     id: movie.id,
     title: movie.title,
     rating: movie.rating,
+    images: {
+      small: movie.images.small,
+      medium: movie.images.medium,
+      large: movie.images.large,
+    },
     year: movie.year,
   };
 };
 
-export const showTransform = (
-  show: DbShow
-): Omit<Show, 'thumbnailRegular' | 'thumbnailTrending'> => {
+export const showTransform = (show: DbShow): Show => {
   return {
     id: show.id,
     title: show.title,
     rating: show.rating,
+    images: {
+      small: show.images.small,
+      medium: show.images.medium,
+      large: show.images.large,
+    },
     year: show.year,
   };
 };
