8d173010efc4bf6013b64bd1e3067656fbd1089d
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-09-05 10:03:29 -0500

    installed codgen dependencies and setup the client to connect to the graphql server on start up, wrapped our app around apollo provider to provide a graphql context to the app, confirmed that a simple query can be made

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+import { GRAPHQL_URI } from '../utils/config';
+import App from './App';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
