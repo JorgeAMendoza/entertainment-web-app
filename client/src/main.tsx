f7ab46b4e1a9e7d22b833f8c75e17c13d33fb39a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-10-01 10:15:37 -0500

    installed react router dom with its type, created a simple route to a generic page to confirm that the application was able to function properly and navigate to that page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,22 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { GRAPHQL_URI } from '../utils/config';
 import App from './App';
 import './index.css';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: GRAPHQL_URI as string,
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([
+  { path: '/', element: <div>Hello world, this is a movie app</div> },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>
 );
