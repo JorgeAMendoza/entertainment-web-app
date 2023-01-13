9088a1cdb2ca5315166c62317ab8178e3a0c42ed
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-01-13 10:32:55 -0600

    move the root route into its own folder with a simpel style applied

--- client/src/routes/Root/Root.tsx
+++ client/src/routes/Root/Root.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import extractToken from '../utils/extract-token';
-import GlobalStyles from '../styles/Globa.styled';
+import extractToken from '../../utils/extract-token';
+import GlobalStyles from '../../styles/Global.styled';
+import RootStyled from './Root.styled';
 
 const Root = () => {
   const navigate = useNavigate();
@@ -18,10 +19,10 @@ const Root = () => {
   }, []);
 
   return (
-    <div id="router-root">
+    <RootStyled id="router-root">
       <GlobalStyles />
       <Outlet />
-    </div>
+    </RootStyled>
   );
 };
 
