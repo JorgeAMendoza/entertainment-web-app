c3fbafe1746da83f8af28a7181c5b0e11e6b320f
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-04-10 06:13:20 -0500

    test user login created

--- server/src/utils/user-data.ts
+++ server/src/utils/user-data.ts
@@ -1,19 +1,11 @@
 import { hashSync } from 'bcrypt';
 
 const userOnePasswordHash = hashSync('Chopper!?990', 10);
-const userTwoPasswordHash = hashSync('Luffy!?2020', 10);
 const userData = [
   {
-    email: 'jorgemendoza2002@gmail.com',
-    name: 'Jorge A. Mendoza II',
+    email: 'testuser@gmail.com',
+    name: 'Monkey D. Luffy',
     passwordHash: userOnePasswordHash,
-    bookMarkedShows: [],
-    bookMarkedMovies: [],
-  },
-  {
-    email: 'isabelmendoza2002@gmail.com',
-    name: 'Isabel Mendoza',
-    passwordHash: userTwoPasswordHash,
     bookMarkedShows: [],
     bookMarkedMovies: [],
   },
