c238636588e5f1bf5d89388731856d64ce7b3f06
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-06-05 05:58:53 -0500

    fix HOST env for development env on client

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -3,4 +3,4 @@ export const GRAPHQL_URI =
   import.meta.env.VITE_GRAPHQL || 'http://localhost:4000/graphql';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const HOST = import.meta.env.VITE_HOST as string || 'http://localhost:4000';
+export const HOST = import.meta.env.VITE_HOST as string || 'http://localhost:4000/';
